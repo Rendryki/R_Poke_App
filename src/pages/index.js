50c3d4ce527cd9e4280201035a64bad7d12e7025
Rendryki Melo <rendrykifelipesilva@gmail.com>
2023-04-17 18:37:15 -0300

    commit fix

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import Navbar from './components/navbar';
+import IndexPageContent from './components/indexPageContent';
+
+export default function Home() {
+  return (
+      <>
+          <Navbar />
+          <IndexPageContent />
+      </>
+  )
+}
